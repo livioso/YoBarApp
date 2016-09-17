aa22ccff014078d146649c0704628ceba3825148
Yves Buschor <yves.buschor@gmail.com>
2016-09-17 09:03:04 +0200

    trying out react-native-swipe-carousel
    https://github.com/jonasantonelli/react-native-swipe-carousel

--- App/Playground.js
+++ App/Playground.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import SwipeCarousel from 'react-native-swipe-carousel';
 
 export const Playground = () => {
   return (
-    <View />
+    <Carousel />
   );
 };
+
+const Carousel = () => (
+  <View>
+    <SwipeCarousel style={{ height: 200 }}>
+      <View style={{ flex: 1, backgroundColor: 'red' }}><Text>one</Text></View>
+      <View style={{ flex: 1, backgroundColor: 'blue' }}><Text>two</Text></View>
+      <View style={{ flex: 1, backgroundColor: 'green' }}><Text>three</Text></View>
+    </SwipeCarousel>
+
+    <SwipeCarousel style={{ height: 200 }}>
+      <View style={{ flex: 1, backgroundColor: 'yellow' }}><Text>one</Text></View>
+      <View style={{ flex: 1, backgroundColor: 'brown' }}><Text>two</Text></View>
+      <View style={{ flex: 1, backgroundColor: '#C0FFEE' }}><Text>three</Text></View>
+    </SwipeCarousel>
+  </View>
+);
