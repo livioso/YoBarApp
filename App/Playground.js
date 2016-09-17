61ce1a5c227ddb3d7b2daf43fdf10c3892d2b20c
Yves Buschor <yves.buschor@gmail.com>
2016-09-17 10:17:36 +0200

    some small cosmetic adjustments to SwipeCarousel

--- App/Playground.js
+++ App/Playground.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import SwipeCarousel from 'react-native-swipe-carousel';
+import SwipeCarousel from './Components/Elements/SwipeCarousel';
 
 export const Playground = () => {
   return (
